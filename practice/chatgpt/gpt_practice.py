6434786f25b63bbb3ac3e3c7af1b14d2d463c90a
thewooniverse <57866696+thewooniverse@users.noreply.github.com>
2023-09-02 23:43:24 +0100

    baseline functioning chatgpt / langchain

--- practice/chatgpt/gpt_practice.py
+++ practice/chatgpt/gpt_practice.py
@@ -1,12 +1,24 @@
 import os
 from dotenv import load_dotenv
-
+import sys
+from langchain.document_loaders import TextLoader
+from langchain.indexes import VectorstoreIndexCreator
+import constants
 
 # load environment keys
-load_dotenv()
-TG_API_KEY = os.getenv('TG_API_KEY')
-OPEN_API_KEY = os.getenv('OPEN_AI_API_KEY')
+# load_dotenv()
+# TG_API_KEY = os.getenv('TG_API_KEY')
+# OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+os.environ['OPENAI_API_KEY'] = constants.APIKEY
 
+
+
+
+query = sys.argv[1]
+loader = TextLoader(f'{os.getcwd()}{os.path.sep}data.txt')
+index = VectorstoreIndexCreator().from_loaders([loader])
+
+print(index.query(query))
 
 
 
